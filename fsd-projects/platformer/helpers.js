376a23637d4bacad2d13cbed09031ae663bf8100
bennett73332 <bennett73332@students.apsb.org>
2026-08-26 13:13:06 +0000

    minor changes 2

--- fsd-projects/platformer/helpers.js
+++ fsd-projects/platformer/helpers.js
@@ -398,14 +398,14 @@ function deathOfPlayer() {
   ctx.fillStyle = "black";
   ctx.font = "800% serif";
   ctx.fillText(
-    "You are dead",
+    "You are dead!",
     canvas.width / 4,
     canvas.height / 6 + canvas.height / 5,
     (canvas.width / 16) * 14
   );
   ctx.font = "500% serif";
   ctx.fillText(
-    "Hit any key to restart",
+    "Hit any key.",
     canvas.width / 4,
     canvas.height / 6 + canvas.height / 3,
     (canvas.width / 16) * 14
@@ -691,7 +691,7 @@ function winGame() {
   );
   ctx.font = "500% serif";
   ctx.fillText(
-    "Hit any key to restart",
+    "Rainbowland is safe.",
     canvas.width / 4,
     canvas.height / 6 + canvas.height / 3,
     (canvas.width / 16) * 14
